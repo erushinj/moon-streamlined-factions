e82d2fc086ff0c9f8eaeea03d1b11e71ae8a1ae6
moon <108492539+erushinj@users.noreply.github.com>
2024-10-08 12:13:13 -0600

    Update Streamlined SWATs to v4.8.1

--- Streamlined SWATs/mod.lua
+++ Streamlined SWATs/mod.lua
@@ -1,37 +1,39 @@
 if not StreamSWATs then
 
-	StreamSWATs = {
-		mod_instance = ModInstance
-	}
+	Global.streamlined_factions = Global.streamlined_factions or {}
+
+	StreamSWATs = ModInstance
 
 	-- Check faction tweaks
-	if not Global.stream_swats_check then
-		Global.stream_swats_check = true
+	if not Global.streamlined_factions.checked_swats then
+		Global.streamlined_factions.checked_swats = true
 
-		if StreamHeist and StreamHeist.settings.faction_tweaks.swat then
+		local sh = StreamHeist
+		if sh and sh.settings and sh.settings.faction_tweaks and sh.settings.faction_tweaks.swat then
 			return
 		end
 
-		StreamSWATs.mod_instance.supermod:GetAssetLoader():LoadAssetGroup("main")
+		StreamSWATs:GetSuperMod():GetAssetLoader():LoadAssetGroup("main")
 	end
 
 end
 
 if RequiredScript == "lib/tweak_data/charactertweakdata" then
 	-- Custom maps often break the character_map, need to be safe when adding to it
-	local logged_error
 	local function safe_add(char_map_table, element)
 		if not char_map_table or not char_map_table.list then
-			if not logged_error then
-				logged_error = true
+			if not Global.streamlined_factions.logged_error then
+				Global.streamlined_factions.logged_error = true
 
-				log("[Streamlined SWATs] WARNING: CharacterTweakData:character_map has missing data! One of your mods uses outdated code, check for mods overriding this function!")
+				log("[Streamlined Factions] WARNING: CharacterTweakData:character_map has missing data! One of your mods uses outdated code, check for mods overriding this function!")
 			end
 
 			return
 		end
 
-		table.insert(char_map_table.list, element)
+		if not table.contains(char_map_table.list, element) then
+			table.insert(char_map_table.list, element)
+		end
 	end
 
 	Hooks:PostHook( CharacterTweakData, "character_map", "streamlined_swats_character_map", function(self)
